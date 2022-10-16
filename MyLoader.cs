30f3220af025e86cfc95d2ade8c32b21b90613d1
Dorlander <61416392+Dorlander@users.noreply.github.com>
2022-10-16 03:19:02 +0300

    Add files via upload

---
+++ MyLoader.cs
@@ -0,0 +1,29 @@
+﻿namespace Flowers_Yasuo
+{
+    #region 
+
+    using EnsoulSharp;
+    using EnsoulSharp.SDK;
+
+    #endregion
+
+    // port my script xD
+    // all logic same with my Yasuo (Aimkek version)
+    // i dont have any update or do some change
+    // just easy port to ensoulsharp
+    public class MyLoader
+    {
+        public static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += () =>
+            {
+                if (ObjectManager.Player.CharacterName != "Yasuo")
+                {
+                    return;
+                }
+
+                new MyBase.MyChampions();
+            };
+        }
+    }
+}
